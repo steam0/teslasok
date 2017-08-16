d3b02aadda9ba36fb68e8bcedcef63e8b5f9a643
Henrik Stene <henrik.stene@knowit.no>
2017-08-16 09:39:38 +0200

    External server

--- js/appRun.js
+++ js/appRun.js
@@ -1,4 +1,4 @@
 myApp.run(['$cookies','$rootScope', 'envService', 'TeslaService', function($cookies, $rootScope, envService, TeslaService) {
-   $rootScope.hostname = "http://localhost:3333/search?";
+   $rootScope.hostname = "https://teslasok-backend.herokuapp.com/search?";
    $rootScope.imageHostname = "https://static-assets.tesla.com/configurator/compositor?";
 }]);
